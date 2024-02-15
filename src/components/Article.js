066d387770765b0c29fa8f18961c3f7d62fca7cf
norahkibathi <norah.njeri@student.moringaschool.com>
2024-02-15 14:05:51 +0300

    completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Article(props) {
+
+  const formattedDate = props.date || "January 1, 1970";
+
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <small>{formattedDate}</small>
+      <p>{props.preview}</p>
+    </article>
+  );
+}
+
+export default Article;
